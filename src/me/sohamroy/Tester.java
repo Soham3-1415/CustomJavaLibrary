cb43dc494da055a23c392aee4bffd68e92a0a913
Soham Roy <soham3.1415@gmail.com>
2018-02-08 00:17:20 -0600

    Created tests

--- src/me/sohamroy/Tester.java
+++ src/me/sohamroy/Tester.java
@@ -6,11 +6,48 @@ import java.math.BigInteger;
 public class Tester {
     static long startTime = 0;
     public static void main(String[] args) {
+        BigFraction testFraction1 = new BigFraction(BigInteger.valueOf(1),BigInteger.valueOf(2));
+        BigFraction testFraction2 = new BigFraction(BigInteger.valueOf(1),BigInteger.valueOf(6));
 
+        startTiming();
+        System.out.println(testFraction1);
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.add(testFraction2));
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.negate());
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.negate().abs());
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.multiply(testFraction2));
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.reciprocal());
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.reciprocal().doubleValue());
+        printTime();
+
+        startTiming();
+        System.out.println(testFraction1.divide(testFraction2));
+        printTime();
+
+        startTiming();
+        System.out.println(new BigFraction(BigDecimal.valueOf(.75),BigDecimal.valueOf(.25)));
+        printTime();
     }
 
     private static void printTime(){
-        System.out.println("Time Elapsed: " + (System.nanoTime() - startTime) / 1000000.0 + " seconds\r");
+        System.out.println("Time Elapsed: " + (System.nanoTime() - startTime) / 1000000.0 + " seconds\n");
     }
 
     private static void startTiming(){
